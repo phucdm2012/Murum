55c8c351e6ceacb0abe4fd603522561e2874ffdb
phucdm <phucdm2012@gmail.com>
2026-07-10 11:54:10 +0700

    Add sidebar and theme manager classes

---
+++ src/widgets/sidebar.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+
+
+class SideBar(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+class SideBarButtom(QWidget):
+    def __init__(self):
+        super().__init__()
---
+++ themes/theme.py
@@ -0,0 +1,5 @@
+
+
+class ThemeManager:
+    def __init__(self):
+        pass
